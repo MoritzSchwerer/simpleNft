7faeb6bb5c4ea2adf7d7a48d05bdb9436caadb70
MoritzSchwerer <moritz.schwerer@gmx.de>
2022-03-16 10:00:09 +0100

    initial commit

---
+++ contracts/SimpleNft.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SimpleNft is ERC721 {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("RewardToken", "RT") {}
+
+  function reward(address receiver/*, string memory tokenURI*/) public returns(uint256) {
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
+    _mint(receiver, newItemId);
+    //_setTokenURI(newItemId, tokenURI);
+    return newItemId;
+  }
+}
